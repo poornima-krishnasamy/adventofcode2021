ce6c2a605c8874e38e0912b7d45a3e58dca67a71
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-12-01 17:39:33 +0000

    Day 1 - Part 2 solution

--- day1/main.go
+++ day1/main.go
@@ -21,4 +21,11 @@ func main() {
 	input := utils.ReadInputFile("input.txt")
 
 	fmt.Println(countIncreased(input))
+	var windowedInput []int64
+	for i := 2; i < len(input); i++ {
+		w := input[i-2] + input[i-1] + input[i]
+		windowedInput = append(windowedInput, w)
+	}
+
+	fmt.Println(countIncreased(windowedInput))
 }
